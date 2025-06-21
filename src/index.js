6c20f2a8624ef9d36cc5ba17f8589783fd4cff64
Sendra237 <Maiva9638@gmail.com>
2025-06-21 17:32:51 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import "./login.css";
+import "./dashboard.css";
+import "./popups.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
